d781e650e8dd988a58009f9b0e0fba57a1e0a18c
German Rosati <german.rosati@gmail.com>
2021-05-17 08:51:10 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,59 @@
+[![](imgs/unsam_factor_data.png)]()
+
+# Visualización de Datos
+# [Germán Rosati](gefero.github.io)
+
+## Presentación
+Docente: Germán Rosati 
+Fechas: 3, 17 y 31 de mayo y 14 y 28 de junio 
+
+Suele pensarse que el proceso de generación de información visualizable en una investigación es un aspecto meramente estético (o sea, secundario) y delegable en otras disciplinas. Sin embargo, la construcción de visualizaciones efectivas ayuda a explorar y comprender diversos aspectos de la estructura subyacente en los datos.
+
+A su vez, son muy relevantes en la comunicación de resultados tanto a audiencias técnicas como no técnicas. La comunicación pública de la ciencia se beneficia de visualizaciones e infografías diseñadas de forma clara. Un buen despliegue visual de la información ayuda a transmitir conceptos complejos.
+
+Este curso corto de doctorado es una introducción práctica a la visualización de datos. Se enfoca en el análisis y presentación de datos reales. A su vez, se cubren algunos aspectos de la dimensiones estética y cognitiva de la percepción gráfica. Se abordará el flujo de trabajo en el proceso de generación de visualizaciones y se trabajarán diferentes tipos de datos: categóricos, cuantitativos, georreferenciados y algunas herramientas para generar visualizaciones interpretables de modelos de regresión.
+
+
+## Objetivos
+Los objetivos principales del curso son que les estudantes 
+
+1. desarrollen e incorporen algunos criterios operativos e intuiciones sobre la construcción de visualizaciones
+2. puedan identificar las características de una visualización útil
+3. adquieran algunos fundamentos sobre la producción de visualización de datos sólida y efectiva utilizando el lenguaje R.
+
+Vale aclarar que no se trata de un curso de capacitación en R, sino que el lenguaje será utilizado como herramienta para generar visualizaciones de forma replicable. 
+
+## Contenidos y materiales
+- __Módulo 0.__ Introducción rápida a R.
+- [](/clase0/notebook/)
+
+- __Módulo 1.__ 
+[](/clase1/notebook/)
+
+- __Módulo 2.__ 
+[](/clase2/notebook/)
+
+- __Módulo 3.__ 
+[](/clase3/notebook/)
+
+- __Módulo 4.__ 
+[](/clase4/notebook/)
+
+
+## Librerías a utilizar
+El taller se desarrollará en R y se hará un uso extensivo de las siguientes librerías:
+
+- `tidyverse`
+
+Pueden instalarse utilizando las instrucciones:
+
+```{r}
+install.packages('tidyverse')  
+```
+
+
+## Bibliografía y sitios de consulta
+
+- TUFTE
+- Healy
+- [Molnar, C. (2020), _Interpretable Machine Learning. A Guide for Making Black Box Models Explainable.](https://christophm.github.io/interpretable-ml-book/)
---
+++ src/0_preproc.R
@@ -0,0 +1,100 @@
+library(tidyverse)
+library(lubridate)
+library(sf)
+
+df <- read_csv('./data/raw/Covid19Casos.csv')
+
+# df <- df %>%
+#         mutate(across(starts_with('fecha_'), ymd))
+
+df <- df %>%
+        filter(residencia_pais_nombre == 'Argentina') %>%
+        mutate( 
+                edad = case_when(
+                        edad_años_meses == "Meses" ~ 0,
+                        TRUE ~ edad),
+                delay_diagnost = as.integer(fecha_diagnostico - fecha_apertura),
+                link = paste(residencia_provincia_id, residencia_departamento_id, sep='')
+                ) %>%
+        filter(nchar(link) == 5) %>%
+        select(id_evento_caso, link, residencia_provincia_nombre:residencia_departamento_nombre,
+               sexo, edad,origen_financiamiento, delay_diagnost,
+               fecha_apertura, sepi_apertura, fecha_inicio_sintomas, fecha_diagnostico, 
+               fecha_internacion:fecha_fallecimiento,
+               clasificacion:clasificacion_resumen)
+        
+        
+inse <- st_read('./data/raw/inse_vs_radio.shp') %>%
+        rename(link=IDDPTO,
+               vuln_sanitaria=VULNERABIL,
+               nse=NSE,
+               ctro_salud=CENTRO_DE_,
+               hospital=HOSPITAL,
+               posta=POSTA_SANI,
+               poblacion=RADIOS_POB,
+               hogares=RADIOS_HOG) %>%
+        mutate(ctro_salud=as.numeric(ctro_salud),
+               hospital=as.numeric(hospital),
+               posta=as.numeric(posta))
+
+inse_agg <-inse %>%
+        st_drop_geometry(.) %>%
+        group_by(link) %>%
+        summarise(
+                pob_depto = sum(poblacion),
+                hogares_depto = sum(hogares),
+                vuln_sanit = mean(vuln_sanitaria),
+                vuln_sanit_sd = sd(vuln_sanitaria, na.rm = TRUE),
+                inse = mean(nse),
+                inse_sd = sd(nse, na.rm = TRUE),
+                dist_hosp = mean(hospital),
+                dist_hosp_sd = sd(hospital, na.rm = TRUE),
+                dist_ctro_salud = mean(ctro_salud),
+                dist_ctro_salud_sd = sd(ctro_salud),
+                dist_posta = mean(posta),
+                dist_posta_sd = sd(posta),
+        )
+
+
+df <- df %>%
+        left_join(inse_agg) %>%
+        drop_na(inse)
+
+write_rds(df, './data/proc/data_covid.rds')
+
+df_agg <- df %>%
+        group_by(link, residencia_provincia_nombre, residencia_departamento_nombre, 
+                 fecha_apertura, sexo, edad, origen_financiamiento, clasificacion_resumen) %>%
+        summarise(inse=mean(inse),
+                  vuln_sanit = mean(vuln_sanit),
+                  dist_hosp = mean(dist_hosp),
+                  dist_ctro_salid = mean(dist_ctro_salud),
+                  dist_posta = mean(dist_posta),
+                  )
+
+
+write_rds(df_agg, './data/proc/covid_agg.rds')
+
+
+df_agg <- df %>%
+        group_by(link, residencia_provincia_nombre, residencia_departamento_nombre, 
+                 fecha_apertura, sexo, edad, origen_financiamiento, clasificacion_resumen) %>%
+        summarise(inse=mean(inse),
+                  vuln_sanit = mean(vuln_sanit),
+                  dist_hosp = mean(dist_hosp),
+                  dist_ctro_salid = mean(dist_ctro_salud),
+                  dist_posta = mean(dist_posta),
+        )
+
+
+write_rds(df_agg, './data/proc/covid_agg.rds')
+
+
+
+
+set.seed(1249)
+sampled <- df %>%
+        group_by(link) %>%
+        sample_frac(0.10)
+
+write_rds(sampled, './data/proc/sample_data_covid.rds')
